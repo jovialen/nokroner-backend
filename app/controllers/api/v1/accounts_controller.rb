bdeccbf50bdb61ce4b792d5157c374318fe1d7c1
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2025-10-14 20:58:05 +0200

    refactor: format files

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,45 +1,45 @@
 class Api::V1::AccountsController < ApplicationController
-    before_action :set_account, only: [:show, :update, :destroy]
+  before_action :set_account, only: %i[show update destroy]
 
-    def index
-        @accounts = Account.all
-        render json: @accounts
+  def index
+    @accounts = Account.all
+    render json: @accounts
+  end
+
+  def show
+    render json: @account
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      render json: @account, status: :created
+    else
+      render json: @account.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @account
+  def update
+    if @account.update(account_params)
+      render json: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-      @account = Account.new(account_params)
+  def destroy
+    @account.destroy
+    head :no_content
+  end
 
-      if @account.save
-        render json: @account, status: :created
-      else
-        render json: @account.errors, status: :unprocessable_entity
-      end
-    end
+  private
 
-    def update
-      if @account.update(account_params)
-        render json: @account
-      else
-        render json: @account.errors, status: :unprocessable_entity
-      end
-    end
+  def set_account
+    @account = Account.find(params[:id])
+  end
 
-    def destroy
-      @account.destroy
-      head :no_content
-    end
-
-    private
-
-    def set_account
-      @account = Account.find(params[:id])
-    end
-
-    def account_params
-      params.require(:account).permit(:name, :description, :balance)
-    end
+  def account_params
+    params.require(:account).permit(:name, :description, :balance)
+  end
 end
--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,45 +1,45 @@
 class Api::V1::TransactionsController < ApplicationController
-    before_action :set_transaction, only: [:show, :update, :destroy]
+  before_action :set_transaction, only: %i[show update destroy]
 
-    def index
-        @transactions = Transaction.all
-        render json: @transactions
+  def index
+    @transactions = Transaction.all
+    render json: @transactions
+  end
+
+  def show
+    render json: @transaction
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if @transaction.save
+      render json: @transaction, status: :created
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @transaction
+  def update
+    if @transaction.update(transaction_params)
+      render json: @transaction, status: :update
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @transaction = Transaction.new(transaction_params)
+  def destroy
+    @transaction.destroy
+    head :no_content
+  end
 
-        if @transaction.save
-            render json: @transaction, status: :created
-        else
-            render json: @transaction.errors, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def update
-        if @transaction.update(transaction_params)
-            render json: @transaction, status: :update
-        else
-            render json: @transaction.errors, status: :unprocessable_entity
-        end
-    end
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
 
-    def destroy
-        @transaction.destroy
-        head :no_content
-    end
-
-    private
-
-    def set_transaction
-        @transaction = Transaction.find(params[:id])
-    end
-
-    def transaction_params
-        params.require(:transaction).permit(:name, :description, :amount)
-    end
+  def transaction_params
+    params.require(:transaction).permit(:name, :description, :amount)
+  end
 end
