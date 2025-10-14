4ee56f91e28f49d300d5bb4814a51a85dd43441a
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2025-10-14 21:57:07 +0200

    fix: formatting issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
-  scope '/api' do
-    match '*path', to: redirect('/api/v1/%{path}'), via: :all, constraints: ->(req) { !req.path.start_with?('/api/v1') }
+  scope "/api" do
+    match "*path",
+          to: redirect("/api/v1/%{path}"),
+          via: :all,
+          constraints: ->(req) { !req.path.start_with?("/api/v1") }
   end
 
   namespace :api do
